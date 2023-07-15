300feb95175a664a270f6977bf6353c53a161278
James Shaun <SHAUNJ@bridgwater.ac.uk>
2023-07-15 12:57:55 +0100

    Initial commit

---
+++ wordle/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <board />
+      <keyboard />
+    </div>
+  );
+}
+
+export default App;
