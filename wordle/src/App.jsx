73faf87381ffdb61a5e7f67c45a42e6361231804
James Shaun <SHAUNJ@bridgwater.ac.uk>
2023-07-15 13:30:20 +0100

    Added board, letters and UseContext

--- wordle/src/App.jsx
+++ wordle/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import Keyboard from "./components/Keyboard";
+import Board from "./components/Board";
+import { boardDefault } from "./words";
+import { createContext } from "react";
 
-import "./App.css";
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <board />
-      <keyboard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
